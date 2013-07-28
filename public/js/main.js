fdb80e579347b5ec7ef616f626e88defcfc1d114
Matthew Daly <matthewbdaly@gmail.com>
2013-07-28 13:37:13 +0100

    Can now view comments

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    'use strict';
+
+    // Declare variables used
+    var messages = [], socket, field, sendButton, content;
+
+    // Set variables
+    socket = io.connect('http://localhost:3700');
+    field = $('input#field');
+    sendButton = $('input#send');
+    content = $('div#content');
+
+    // Handle new posts
+    socket.on('message', function (data) {
+        if (data.message) {
+            messages.push(data.message);
+            var html = '';
+            _.each(messages, function (message) {
+                html += message + '<br />';
+            });
+            content.html(html);
+        } else {
+            console.log('There is a problem:', data);
+        }
+    });
+
+    sendButton.on('click', function () {
+        var text = field.val();
+        socket.emit('send', { message: text });
+    });
+});
